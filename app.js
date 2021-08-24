49db9e2c6dbadeab592755a44a43eb83554c5ac9
massdo <dorian_massoulier@hotmail.com>
2021-08-24 21:13:29 +0200

    adding 404 error page message PORT is now configurable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
-const port = 3000
+const notFound = require('./middleware/not-found')
 require('dotenv').config()
 // static files
 app.use(express.static('public'))
@@ -10,9 +10,11 @@ app.use(express.json())
 
 // route
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
 
 // faire une fonction start asynchrone pour démarrer le serveur uniquement si la DB est connectée
 
+const port = process.env.PORT || 3000
 const start = async ()=>{
     try {
         await connectDB(process.env.MONGO_URI)
--- routes/tasks.js
+++ routes/tasks.js
@@ -18,6 +18,4 @@ const {
 router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
-
-
 module.exports = router
